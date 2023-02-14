c1f01242acb5c7b2384b7e72e003fe39f8868afc
Aldrex99 <a.casu99@gmail.com>
2023-02-14 14:59:02 +0100

    test

---
+++ test/test.js
@@ -0,0 +1,1 @@
+const assert = require("chai").assert;  function add(x, y) {   return x + y; }  describe("add", function () {   it("should add two numbers", function () {     assert.equal(add(1, 2), 3);   }); });
